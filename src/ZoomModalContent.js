f1e621118f33dd90b44424efe9acc045bf36b9fb
r22n <r22n@github.com>
2023-12-13 21:39:11 +0900

    refactor: smarter css styles

--- src/ZoomModalContent.js
+++ src/ZoomModalContent.js
@@ -12,53 +12,37 @@ export class ZoomModalContent {
 
 	_createContent() {
 		const zoom = `
-			<div class="vjs-zoom-duck__container--row">
-				<button id="vjs-zoom-duck__zoomIn" class="vjs-zoom-duck__button">
-					<span class="vjs-zoom-icons">add</span>
-				</button>
-				<span class="vjs-zoom-duck__space"></span>
-				<button id="vjs-zoom-duck__zoomOut" class="vjs-zoom-duck__button">
-					<span class="vjs-zoom-icons">remove</span>
-				</button>
-			</div>
+			<button id="vjs-zoom-duck__zoomIn" class="vjs-zoom-duck__button" style="grid-area: plus">
+				<span class="vjs-zoom-icons">add</span>
+			</button>
+			<button id="vjs-zoom-duck__zoomOut" class="vjs-zoom-duck__button" style="grid-area: minus">
+				<span class="vjs-zoom-icons">remove</span>
+			</button>
 		`;
 		const move = `
-			<div class="vjs-zoom-duck__container--row">
-				<span class="vjs-zoom-duck__space"></span>
-				<button id="vjs-zoom-duck__moveUp" class="vjs-zoom-duck__button">
-					<span class="vjs-zoom-icons">arrow_drop_up</span>
-				</button>
-				<span class="vjs-zoom-duck__space"></span>
-			</div>
-			<div class="vjs-zoom-duck__container--row">
-				<button id="vjs-zoom-duck__moveLeft" class="vjs-zoom-duck__button">
-					<span class="vjs-zoom-icons">arrow_left</span>
-				</button>
-				<button id="vjs-zoom-duck__reset" class="vjs-zoom-duck__button">
-					<span class="vjs-zoom-icons">fiber_manual_record</span>
-				</button>
-				<button id="vjs-zoom-duck__moveRight" class="vjs-zoom-duck__button">
-					<span class="vjs-zoom-icons">arrow_right</span>
-				</button>
-			</div>
-			<div class="vjs-zoom-duck__container--row">
-				<span class="vjs-zoom-duck__space"></span>
-				<button id="vjs-zoom-duck__moveDown" class="vjs-zoom-duck__button">
-					<span class="vjs-zoom-icons">arrow_drop_down</span>
-				</button>
-				<span class="vjs-zoom-duck__space"></span>
-			</div>
+			<button id="vjs-zoom-duck__moveUp" class="vjs-zoom-duck__button" style="grid-area: up">
+				<span class="vjs-zoom-icons">arrow_drop_up</span>
+			</button>
+			<button id="vjs-zoom-duck__moveLeft" class="vjs-zoom-duck__button" style="grid-area: left">
+				<span class="vjs-zoom-icons">arrow_left</span>
+			</button>
+			<button id="vjs-zoom-duck__reset" class="vjs-zoom-duck__button" style="grid-area: reset">
+				<span class="vjs-zoom-icons">fiber_manual_record</span>
+			</button>
+			<button id="vjs-zoom-duck__moveRight" class="vjs-zoom-duck__button" style="grid-area: right">
+				<span class="vjs-zoom-icons">arrow_right</span>
+			</button>
+			<button id="vjs-zoom-duck__moveDown" class="vjs-zoom-duck__button" style="grid-area: down">
+				<span class="vjs-zoom-icons">arrow_drop_down</span>
+			</button>
 		`;
 		const rotateFlip = `
-			<div class="vjs-zoom-duck__container--row">
-				<button id="vjs-zoom-duck__rotate" class="vjs-zoom-duck__button">
-					<span class="vjs-zoom-icons">rotate_left</span>
-				</button>
-				<span class="vjs-zoom-duck__space"></span>
-				<button id="vjs-zoom-duck__flip" class="vjs-zoom-duck__button">
-					<span class="vjs-zoom-icons">swap_horiz</span>
-				</button>
-			</div>
+			<button id="vjs-zoom-duck__rotate" class="vjs-zoom-duck__button" style="grid-area: rotate">
+				<span class="vjs-zoom-icons">rotate_left</span>
+			</button>
+			<button id="vjs-zoom-duck__flip" class="vjs-zoom-duck__button" style="grid-area: flip">
+				<span class="vjs-zoom-icons">swap_horiz</span>
+			</button>
 		`;
 
 		this.content = `
